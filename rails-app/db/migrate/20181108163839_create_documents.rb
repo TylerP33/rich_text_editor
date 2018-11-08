5a8271394e77fc64126cb2e8e6f612bbb3680659
Tyler Pulse <32463199+TylerP33@users.noreply.github.com>
2018-11-08 11:41:47 -0600

    First Commit

---
+++ rails-app/db/migrate/20181108163839_create_documents.rb
@@ -0,0 +1,11 @@
+class CreateDocuments < ActiveRecord::Migration[5.2]
+  def change
+    create_table :documents do |t|
+      t.text :body
+      t.string :title
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
