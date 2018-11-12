ab32ef412acbca68b014ece62409bb44ee31a3ab
Tyler Pulse <32463199+TylerP33@users.noreply.github.com>
2018-11-12 10:10:10 -0600

    Add FontColor component
    Add FontColor Component for inline styling.
    
    Needs work

---
+++ react-app/src/components/text_editor/FontColor.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+
+class FontColor extends Component {
+
+	render() {
+		return(
+			<div>
+				<h1>{this.props.editorState}</h1>
+			</div>
+		)
+	}
+}
+
+
+export default FontColor
+
+
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import manageDocuments from './reducers/manageDocumentState'
+import docsReducer from './reducers/docsReducer'
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore} from 'redux';
 import * as serviceWorker from './serviceWorker';
 
 
-const store = createStore(manageDocuments, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const store = createStore(docsReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
   <Provider store={store}>
