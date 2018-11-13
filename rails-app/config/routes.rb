dfd5e53eb0430ebfddaa6467e75479a69ec89b9e
Tyler Pulse <32463199+TylerP33@users.noreply.github.com>
2018-11-13 12:07:33 -0600

    Update rails-app/config/routes.rb
    Testing create functionality (post) in Rails API

--- rails-app/config/routes.rb
+++ rails-app/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users
-      resources :documents  
+      resources :documents, only: [:new, :index, :create, :update, :show]  
     end
   end
 end
