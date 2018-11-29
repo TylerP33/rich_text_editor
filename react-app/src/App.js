413ecf9511dd5c5309ce4b7d3c5cbfc8f921edf5
Tyler Pulse <32463199+TylerP33@users.noreply.github.com>
2018-11-29 09:27:03 -0600

    Removed component

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import TextEditor from './containers/TextEditor'
-import SavedDocuments from './containers/SavedDocuments'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
           <TextEditor />
-          <SavedDocuments />
       </div>
     );
   }
