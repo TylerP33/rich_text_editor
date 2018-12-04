2437bc0e12cf3ac92337f2edfd677f55c223b783
Tyler Pulse <32463199+TylerP33@users.noreply.github.com>
2018-12-04 15:17:35 -0600

    Added ReactRouter

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
+// react router
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+// containers
 import TextEditor from './containers/TextEditor'
 import Analytics from './containers/Analytics'
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <Analytics />
-      </div>
+     <Router>
+  		<div className="app">
+  			<div>
+				<Route exact path='/' component={Analytics} />
+				<Route exact path='/analytics' component={Analytics} />
+				<Route exact path='/editor' component={TextEditor} />
+			</div>
+    	</div>
+   	  </Router> 
     );
   }
 }
