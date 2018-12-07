8c78350bce8e6ccb7f1ebd87f97c551249d407dc
Tyler Pulse <32463199+TylerP33@users.noreply.github.com>
2018-12-06 19:02:49 -0600

    FirstLetter works as intended

--- react-app/src/components/FirstLetter/AlphabetClick.js
+++ react-app/src/components/FirstLetter/AlphabetClick.js
@@ -6,24 +6,33 @@ import { NavLink } from 'react-router-dom';
 
 
 class AlphabetClick extends Component {
+	state = {
+		words: ''
+	}
 
+	componentWillMount = () => {
+		console.log(this.state.words)
+	}
 
 	onClick = (e) => {
+		let loopArray = []
 		const currentLetter = e.currentTarget.classList[1][3]
 		if (e.currentTarget.classList.contains(`fa-${currentLetter}`)) {
 			const records = this.props.records
 			let text = []
 			const content = JSON.parse(records[records.length - 1].body.replace(/=>/g, ":")).blocks.forEach(function(object){text.push(object.text)})
-			const splitArr = text.join(" ").split(" ")
-			for (let i = 0; i < splitArr.length; i++){
-				if (splitArr[i][0] === currentLetter) {
-					console.log(splitArr[i])
+			const splitWords = text.join(" ").split(" ")
+			for (let i = 0; i < splitWords.length; i++){
+				if (splitWords[i][0] === currentLetter) {
+					loopArray.push(splitWords[i])
 				}
 			}
 		}
+		this.setState({words: loopArray.join(", ")})
 	}
 
 	render(){
+	console.log(this.state.test)
 	return(
 			<div id="letter-container">
 				<div class="letter">
@@ -124,6 +133,11 @@ class AlphabetClick extends Component {
 				<div class="letter">
 					<span id="letter-span" onClick={this.onClick}  class="fa fa-z"></span><br/>
 				</div>
+			<div id="returned-words">
+			<br />
+
+				{(this.state.words) ? this.state.words : <h1 id="special-h1">Sorry, no words start with this letter!</h1> }
+			</div>
 			</div>
 			)
 	}
